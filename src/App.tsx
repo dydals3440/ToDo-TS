9c88f6f283c2be9598a68654d3ce4511b5cd6964
김용민 <dydals3440@gmail.com>
2023-08-24 20:48:44 +0900

    feat: Function Props로 작업하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
   const todos = [new Todo('Learn React'), new Todo('learn TS')];
+  const addTodoHandler = (todoText: string) => {};
   return (
     <div>
+      {/* addTodoHandler 포인터를 newTodo에 전달해야함! */}
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
