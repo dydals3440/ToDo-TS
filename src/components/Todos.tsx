2f9605819d7e176a834313a18912e838443a66a1
김용민 <dydals3440@gmail.com>
2023-08-24 20:04:35 +0900

    feat: TodoItem 컴포넌트 타입스크립트 제작

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 // React.FC '@types/react' 패키지에 정의된 내용(Functional Components)
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
